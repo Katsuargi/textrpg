d78b0794ee8da1165d4d2f91d85f0eb832a96212
Lee <Katsuargi@gmail.com>
2018-01-25 18:46:48 -0500

    stats are working.

--- js/storestats.js
+++ js/storestats.js
@@ -2,28 +2,38 @@ var test3;
 var test4;
 var test5;
 var totalStats;
+var health;
 
 function storeTest() {
-    test3 = parseInt(document.getElementById("str").value);
-    test4 = parseInt(document.getElementById("dex").value);
-    test5 = parseInt(document.getElementById("sta").value);
-
-    totalStats = test3 + test4 + test5;
-    console.log(test3, test4, test5, totalStats);
-    if (test3 > 10) {
+    playern = (document.getElementById("playerin").value);
+    baseStr = parseInt(document.getElementById("str").value);
+    baseDex = parseInt(document.getElementById("dex").value);
+    baseSta = parseInt(document.getElementById("sta").value);
+    
+    totalStats = baseStr + baseDex + baseSta;
+    console.log(playern, baseStr, baseDex, baseSta, totalStats);
+    if (baseStr > 10) {
     	alert("Your strength is above 10. Please enter a value between 1-10");
     }
-    else if (test4 > 10) {
+    else if (baseDex > 10) {
     	alert("Your dexterity is above 10. Please enter a value between 1-10");
     }
-    else if (test5 > 10) {
+    else if (baseSta > 10) {
     	alert("Your stamina is above 10. Please enter a value between 1-10");
     }
     else if (totalStats > 15) {
     	alert("Your total stats are above 15. Please lower your stats.")
     }
     else {
-    	alert("Looking good!");
+        health = baseSta*10;
+        document.getElementById("playern").innerHTML=playern;
+        document.getElementById("healthdisplay").innerHTML=health;
+        document.getElementById("strdisplay").innerHTML=baseStr;
+        document.getElementById("dexdisplay").innerHTML=baseDex;
+        document.getElementById("stadisplay").innerHTML=baseSta;
+        document.getElementById("textrpg").innerHTML=document.getElementById("castle1");
     }
+
 }
+
 
